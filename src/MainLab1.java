cba0fbeedacaf88925beef3f1e8f2a755d5505b8
dmitry <dmitry.trok@gmai.com>
2014-09-06 12:23:14 +0400

    some optemizations

--- src/MainLab1.java
+++ src/MainLab1.java
@@ -2,16 +2,16 @@ import java.util.Vector;
 
 public class MainLab1 {
 	public static void main(String[] args) {
-
-		if(args.length < 2){
+		int elem_count = args.length;
+		if(elem_count < 2){
 			System.out.print("What do you want to sort ?\n");
 			System.out.print("Input only integer numbers\n");
 			System.out.print("Example:java MainLab1 5 4 3 2 1\n");
 			System.exit(1);
 		}
 
-		Vector<Integer> data = new Vector<Integer>(args.length);
-		for(int i = 0; i < args.length; i++)
+		Vector<Integer> data = new Vector<Integer>(elem_count);
+		for(int i = 0; i < elem_count; i++)
 			data.add(Integer.parseInt(args[i]));
 
 		System.out.print("Input : " + data + "\n");
