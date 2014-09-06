8e1524e0d81195cf1ac5082f0c365e03ffe1fa59
dmitry <dmitry.trok@gmai.com>
2014-09-06 13:38:28 +0400

    Add exeption handler

--- src/MainLab1.java
+++ src/MainLab1.java
@@ -1,11 +1,19 @@
 import java.util.Vector;
 
 public class MainLab1 {
+	public static void printHelp(){
+		
+	}
 	public static void main(String[] args) {
 		Vector<Integer> data = new Vector<Integer>(args.length);
-		for(int i = 0; i < args.length; i++)
-			data.add(Integer.parseInt(args[i]));
-
+		try {
+			for(int i = 0; i < args.length; i++)
+				data.add(Integer.parseInt(args[i]));
+		} 
+		catch(NumberFormatException e) {
+			System.out.println("Only integer number support");
+			System.exit(1);			
+		}
 		System.out.print("Input : " + data + "\n");
 		BubleSort.sort(data);
 		System.out.print("Output: " + data + "\n");	
