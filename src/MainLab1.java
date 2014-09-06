5d0bbfff90e941b930947cbecb41936557b2882a
dmitry <dmitry.trok@gmai.com>
2014-09-06 12:16:41 +0400

    add exception hendling for input agruments by count check

--- src/MainLab1.java
+++ src/MainLab1.java
@@ -2,6 +2,14 @@ import java.util.Vector;
 
 public class MainLab1 {
 	public static void main(String[] args) {
+
+		if(args.length < 2){
+			System.out.print("What do you want to sort ?\n");
+			System.out.print("Input only integer numbers\n");
+			System.out.print("Example:java MainLab1 5 4 3 2 1\n");
+			System.exit(1);
+		}
+
 		Vector<Integer> data = new Vector<Integer>(args.length);
 		for(int i = 0; i < args.length; i++)
 			data.add(Integer.parseInt(args[i]));
