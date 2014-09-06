40b1d35e604ffdb1dbabff7a3c04a210e708445f
dmitry <dmitry.trok@gmai.com>
2014-09-06 11:35:59 +0400

    chanje print -> println

--- src/MainLab1.java
+++ src/MainLab1.java
@@ -1,7 +1,5 @@
-package ru.spbstu.telematics.dLebedev.lab01;
-
 public class MainLab1 {
 	public static void main(String[] args) {
-		System.out.print("hello");
+		System.out.println("hello");
 	}
 }
