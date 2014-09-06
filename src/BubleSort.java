58b285136929a678c72915b5799a7725ca55f230
dmitry <dmitry.trok@gmai.com>
2014-09-06 12:12:32 +0400

    write buble sort method

--- src/BubleSort.java
+++ src/BubleSort.java
@@ -2,6 +2,16 @@ import java.util.Vector;
 
 public class BubleSort {
 	public static Vector<Integer> sort(Vector<Integer> data){
+		int size = data.size();
+		for(int i = 0; i < size - 1; i++)
+			for(int j = 0; j < size - i - 1; j++){
+				Integer a = data.get(j);
+				Integer b = data.get(j + 1);
+				if(a > b){
+					data.set(j, b);
+					data.set(j + 1, a);
+				}
+			}
 		return data;
 	}
 }
