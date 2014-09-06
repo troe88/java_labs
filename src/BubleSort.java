9924b0ed3ecba1e656bf812bc824802ecfecec81
dmitry <dmitry.trok@gmai.com>
2014-09-06 11:57:41 +0400

    add test function

---
+++ src/BubleSort.java
@@ -0,0 +1,10 @@
+import java.util.Vector;
+
+public class BubleSort {
+	public static Vector<Integer> sort(Vector<Integer> data){
+		return data;
+	}
+	public static void test(){
+		System.out.println("qweasdzxc");
+	}
+}
--- src/MainLab1.java
+++ src/MainLab1.java
@@ -1,5 +1,6 @@
 public class MainLab1 {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		BubleSort.test();
 	}
 }
