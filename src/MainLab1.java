ffa01aee08b13262b6ef90da67324f223ee23c60
dmitry <dmitry.trok@gmai.com>
2014-09-06 13:48:06 +0400

    Add help and some exeptions

--- src/MainLab1.java
+++ src/MainLab1.java
@@ -2,15 +2,19 @@ import java.util.Vector;
 
 public class MainLab1 {
 	public static void printHelp(){
-		
+		System.out.print("Input only 2 or more integer numbers\n");
+		System.out.print("Example:java MainLab1 3 1 4 5\n");
 	}
 	public static void main(String[] args) {
+
 		Vector<Integer> data = new Vector<Integer>(args.length);
 		try {
+			if(args.length < 2)
+				throw new IllegalArgumentException();
 			for(int i = 0; i < args.length; i++)
 				data.add(Integer.parseInt(args[i]));
 		} 
-		catch(NumberFormatException e) {
+		catch(Exception e) {
 			printHelp();
 			System.exit(1);			
 		}
